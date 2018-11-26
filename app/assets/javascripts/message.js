904ceeffc14719718d219f332a72c9163f867926
dattsu <dattsu@dattsu-MacBook-Pro.local>
2018-11-26 14:33:05 +0900

    Asynchronous communication２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,32 +1,22 @@
 $(function() {
   function buildHTML(message){
-    if ( message.image ) {
-      var html = `<div class= "main__message-content__box" >
-                    <div class= "main__message-content__box__user-name" >${message.user_name}
-                    </div>
-                    <div class = "main__message-content__box__time" >${message.created_at})
-                    </div>
-                    <div class ="main__message-content__box__text" >${message.body}
-                    </div>
-                      <asset_path src=${message.image_url} >
-                  </div>`
-    } else {
-      var html = `<div class= "main__message-content__box" >
-                    <div class= "main__message-content__box__user-name" >${message.user_name}
-                    </div>
-                    <div class = "main__message-content__box__time" >${message.created_at})
-                    </div>
-                    <div class ="main__message-content__box__text" >${message.body}
-                    </div>
-                  </div>`
-      return html;
-    };
-  }
 
+  var image = message.is_image_present ? `<img src='${message.image.url}'> ` : ''
+
+    var html = `<div class= "main__message-content__box" >
+                  <div class= "main__message-content__box__user-name" >${message.user_name}
+                  </div>
+                  <div class = "main__message-content__box__time" >${message.created_at})
+                  </div>
+                  <div class ="main__message-content__box__text" >${message.body}
+                  </div>
+                     ${image}
+                </div>`
+    return html;
+  }
 
   $('.form__message_send').on('submit', function(e){
       e.preventDefault();
-      // var $form = $(this)
       var formData = new FormData(this);
       var url = $(this).attr('action')
       $.ajax({
@@ -39,18 +29,16 @@ $(function() {
       })
         .done(function(data){
           var html = buildHTML(data);
-          console.log(data)
           $messages = $(".main__message-content");
           $messages.append(html);
           $('form')[0].reset();
           $('.form__message_send').val('');
-          $messages.animate({
-            scrollTop: $messages[0].scrollHeight}, 'fast'
-           );
+          $messages.animate({scrollTop: $messages[0].scrollHeight}, 'fast');
         })
-         .fail(function(){
+         .fail(function(message){
            alert('error');
          });
          return false;
          });
 });
+
