397b9bafc1dc358136eef67010c42ed8f8d4b5d5
dattsu <dattsu@dattsu-MacBook-Pro.local>
2018-12-01 18:58:07 +0900

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ set :application, 'chat-space'
 set :repo_url,  'git@github.com:dattsu/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
+
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
@@ -21,8 +23,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
 
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
