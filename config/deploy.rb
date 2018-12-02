13e65e320840e80ce55a4aa610e37672e2c6a2e7
dattsu <dattsu@dattsu-MacBook-Pro.local>
2018-12-02 14:58:27 +0900

    edit_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
+set :linked_files, %w{ config/secrets.yml }
+
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
@@ -5,7 +14,6 @@ set :application, 'chat-space'
 set :repo_url,  'git@github.com:dattsu/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, %w{ config/secrets.yml }
 
 
 set :rbenv_type, :user
