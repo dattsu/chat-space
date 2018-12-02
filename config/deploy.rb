d40aa928647ef14a904952711cc87eb6338908a5
dattsu <dattsudaaa@gmail.com>
2018-12-02 19:14:06 +0900

    Edit_deploy.rb5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,23 @@
 # config valid only for current version of Capistrano
 lock '~> 3.11.0'
 
-# secrets.yml用のシンボリックリンクを追加
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:dattsu/chat-space.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, %w{ config/secrets.yml }
 
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/key_pair3.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -24,3 +37,10 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
